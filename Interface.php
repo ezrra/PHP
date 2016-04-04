3c2e98a6ef2cf9e54643ade381eb42f680a5ddcb
Israel Martinez <israel.martinez.vargas@gmail.com>
2016-04-04 18:52:12 +0000

    Interface & Abstract class

---
+++ Interface.php
@@ -0,0 +1,14 @@
+<?php
+
+interface MyInterface {
+
+    // Not attributes, alwayse public methods
+    public function method1($params);
+}
+
+class MyClass implements MyInterface {
+
+    public function method1 ($params) {
+
+    }
+}
